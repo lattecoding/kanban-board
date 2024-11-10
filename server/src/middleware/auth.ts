985fcc25459c7bae8a906c5266683f977cefa71d
lattepunk-coding <174910592+lattepunk-coding@users.noreply.github.com>
2024-11-10 16:41:19 -0500

    first attempt at auth.ts middleware

--- server/src/middleware/auth.ts
+++ server/src/middleware/auth.ts
@@ -1,10 +1,36 @@
-import { Request, Response, NextFunction } from 'express';
-import jwt from 'jsonwebtoken';
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+
+declare global {
+  namespace Express {
+    export interface Request {
+      user?: JwtPayload;
+    }
+  }
+}
 
 interface JwtPayload {
   username: string;
 }
 
-export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
-  // TODO: verify the token exists and add the user data to the request object
+export const authenticateToken = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({ message: "Token missing or malformed" });
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET as string, (err, user) => {
+    if (err) {
+      return res.status(403).json({ message: "Invalid or expired token" });
+    }
+
+    req.user = user as JwtPayload;
+    next();
+  });
 };
